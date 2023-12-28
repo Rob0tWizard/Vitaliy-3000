1d1765d3699d4525e193f592d5007fb96e10b506
Rob0tWizard <ilia.dementev65@gmail.com>
2023-12-28 11:32:06 +0700

    project Vitaliy 3000 model: CustomUser Serializer: UserSerializer Admin register:CustomUser

---
+++ vitality/customers/models.py
@@ -0,0 +1,31 @@
+from django.core.validators import validate_email
+from django.db import models
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser, PermissionsMixin
+
+
+class CustomUser(AbstractUser, PermissionsMixin):
+    email = models.EmailField(unique=True, max_length=50, blank=True, null=True, validators=[validate_email])
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(unique=True, max_length=50)
+    date_of_birth = models.DateField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        blank=True,
+        related_name='custom_user_permissions',
+    )
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        blank=True,
+        related_name='custom_user_groups',
+    )
+
+    def __str__(self):
+        return str(self.username) if self.username else str(self.pk)
+
+
